f9ef6f732610b6fa01ae637a469c906d7c99e128
amberorenstein <amber.orenstein@gmail.com>
2017-06-01 22:08:10 -0600

    basic functions work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ var answerPicked;
 var countdownTimer;
 var correctTotal = 0;
 var incorrectTotal = 0;
-var triesLeft = 3;
+
 
 
 //Function that creates the Start Button
@@ -28,34 +28,58 @@ $(".start-button").on("click", function(event){
 	runTimer();
 });
 
+
 //Function to provides question
 function fillDisplay() {
-	gameDisplay = "<p class='text-center timer-p'>Time Remaining: <span class ='timer'>20</span></p><p class = 'text-center'>" + questionArray[questionCounter] + "</p><p class = 'first answer'>1. " + answerArray[questionCounter][0] + "</p><p class = 'second answer'>2. " + answerArray[questionCounter][1] + "</p><p class = 'third answer'>3. " + answerArray[questionCounter][2] + "</p><p class = 'fourth answer'>4. " + answerArray[questionCounter][3] + "</p>";
+	gameDisplay = "<p class='text-center timer-p'>Time Remaining: <span class ='timer'>20</span></p><p class = 'text-center'>" + questionArray[questionCounter] + "</p><p class ='answer'>1. " + answerArray[questionCounter][0] + "</p><p class = 'answer'>2. " + answerArray[questionCounter][1] + "</p><p class = 'answer'>3. " + answerArray[questionCounter][2] + "</p><p class = 'answer'>4. " + answerArray[questionCounter][3] + "</p>";
 	$(".main-space").html(gameDisplay);
+}
+
+
+//Function to switch to next question
+function nextQuestion(){
+	if(questionCounter < 10) {
+		questionCounter++;
+		fillDisplay();
+		counter = 20;
+		runTimer();
+	}
+	else {
+		endGame();
+	}
 }
 
 //Function to count correct answer choice
 function countCorrect() {
 	correctTotal++;
-	gameDisplay = "<p class= 'text-center timer-p'>Time Remaining: <span class = 'timer'>" + counter + "</span></p>" + "<p class = 'text-center'>Correct! The answer is : " + correctAnswers[questionCounter] + "</p>";
-	$(".main-space").html(gameDisplay);
-	setTimeout(next(3000));
+	// gameDisplay = "<p class= 'text-center timer-p'>Time Remaining: <span class = 'timer'>" + counter + "</span></p>" + "<p class = 'text-center'>Correct! The answer is : " + correctAnswers[questionCounter] + "</p>";
+	// $(".main-space").html(gameDisplay);
+	console.log("correct answers = " + correctTotal);
+	alert("CORRECT!");
+	setTimeout(3000);
+	nextQuestion();
 }
 
 //Function to count incorrect answer choice
 function countIncorrect() {
 	incorrectTotal++;
-	gameDisplay = "<p class= 'text-center timer-p'>Time Remaining: <span class = 'timer'>" + counter + "</span></p>" + "<p class = 'text-center'>Incorrect! The correct answer is : " + correctAnswers[questionCounter] + "</p>";
-	$(".main-space").html(gameDisplay);
-	setTimeout(next(3000));
+	console.log("incorrect total = " + incorrectTotal);
+	// gameDisplay = "<p class= 'text-center timer-p'>Time Remaining: <span class = 'timer'>" + counter + "</span></p>" + "<p class = 'text-center'>Incorrect! The correct answer is : " + correctAnswers[questionCounter] + "</p>";
+	// $(".main-space").html(gameDisplay);
+	alert("NOPE!");
+	setTimeout(3000);
+	nextQuestion();
 }
 
 //Function when time runs out
 function timeoutLoss() {
-	gameDisplay = "<p class = 'test-center timer-p'>Time Remaining: <span class = 'timer'>" + counter + "</span></p>" + "<p class = 'text-center'>Time's up! The correct answer was: "+correctAnswers[questionCounter] + "</p>";
+	// gameDisplay = "<p class = 'test-center timer-p'>Time Remaining: <span class = 'timer'>" + counter + "</span></p>" + "<p class = 'text-center'>Time's up! The correct answer was: "+correctAnswers[questionCounter] + "</p>";
 	incorrectTotal++;
-	$(".main-space").html(gameDisplay);
-	setTimeout(next(3000));
+	// $(".main-space").html(gameDisplay);
+	console.log("incorrect total = " + incorrectTotal);
+	alert("TIME'S UP!");
+	setTimeout(3000);
+	nextQuestion();
 }
 
 //Function for the end of the game
@@ -80,24 +104,14 @@ function runTimer() {
 	}
 }
 
-//Function to switch to next question
-function next(){
-	if(questionCounter < 10) {
-		questionCounter++
-		fillDisplay();
-		counter = 20;
-		runTimer();
-	}
-	else {
-		endGame();
-	}
-}
 
 //Click events for picking answers
 $("body").on("click", ".answer", function(event){
 	answerPicked = $(this).text();
-
-	if(answerPicked === correctAnswers[questionCounter]) {
+	answerPicked = answerPicked.substr(3);
+	console.log("new:" + answerPicked);
+	console.log(correctAnswers[questionCounter]);
+	if(answerPicked == correctAnswers[questionCounter]) {
 		countCorrect();
 	}
 
@@ -107,6 +121,7 @@ $("body").on("click", ".answer", function(event){
 	}
 
 });
+
 
 
 });
